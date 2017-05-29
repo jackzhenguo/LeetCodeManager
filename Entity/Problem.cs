88dc03a56fda33b00ad3727cdb600b7a58479e73
jackz <guozhennianhua@163.com>
2017-05-29 22:51:40 +0800

    problem entity revises

--- Entity/Problem.cs
+++ Entity/Problem.cs
@@ -12,10 +12,12 @@ namespace LeetcodeManager.Entity
         public int ProblemId { get; set; }
         [MaxLength(50)]
         [Required(ErrorMessage = "Title is required.")]
+        public string Number { get; set; }
         public string Title { get; set; }
         public string LtUrl { get; set; }
         public string CsdnAddress { get; set; }
         public string Content { get; set; }
-        public virtual IList<Tag> Tags { get; set; }
+        public string TagsCombine { get; set; }//it's separated by ';'
+        public virtual IList<Tag> Tags { get; set; } //it's a navigation property
     }
 }
